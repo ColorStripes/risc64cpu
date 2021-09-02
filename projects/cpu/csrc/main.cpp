c40460ba92c06363a7c066144d9ac4ce7acca940
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-02 16:27:51 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 0d58ca3160334bcfa96936f87b5b7e93     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  16:27:51 up 53 min,  1 user,  load average: 0.47, 0.48, 0.38

--- projects/cpu/csrc/main.cpp
+++ projects/cpu/csrc/main.cpp
@@ -4,11 +4,11 @@
 #include <verilated_vcd_c.h>    
 #include <iostream>
 #include <fstream>
-#include "Vrvcpu1.h"
+#include "Vrvcpu.h"
 
 using namespace std;
 
-static Vrvcpu1* top;
+static Vrvcpu* top;
 static VerilatedVcdC* tfp;
 static vluint64_t main_time = 0;
 static const vluint64_t sim_time = 1000;
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
   Verilated::commandArgs(argc, argv);
   Verilated::traceEverOn(true);
 
-	top = new Vrvcpu1;
+	top = new Vrvcpu;
   tfp = new VerilatedVcdC;
 
   top->trace(tfp, 99);
