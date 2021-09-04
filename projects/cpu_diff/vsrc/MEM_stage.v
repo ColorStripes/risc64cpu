592391a6fb82615c0882234c877b97d3b0624efe
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-05 01:50:57 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 770b95c4b0544c3da8f09e5429ab96ae     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  01:50:57 up  4:37,  1 user,  load average: 0.50, 0.38, 0.25

---
+++ projects/cpu_diff/vsrc/MEM_stage.v
@@ -0,0 +1,107 @@
+//2021.8.5
+//xu xin
+`include "defines.v"
+
+module MEM_stage (
+    input wire rst,
+    input wire [`REG_BUS] ex_w_data,
+    input wire ex_w_ena,
+    input wire [4 : 0] ex_w_addr,
+    
+    input wire [`REG_BUS] ex_mem_waddr,
+    input wire [`REG_BUS] ex_mem_raddr,
+    input wire [`REG_BUS] ex_stor_data,
+    input wire [4 : 0] ex_memop,
+
+    input wire ex_mem_wr,
+    input wire ex_mem_ena,
+    input wire [`REG_BUS] mem_data,
+
+    output reg [`REG_BUS] mem_w_data,
+    output reg mem_w_ena,
+    output reg [4 : 0] mem_w_addr,
+
+    output reg [`REG_BUS] mem_mem_waddr,
+    output reg [`REG_BUS] mem_mem_raddr,
+    output reg [7 : 0] mem_sel,
+    output reg [`REG_BUS] mem_stor_data,
+    output reg mem_wr,
+    output reg mem_mem_ena
+);
+    always @(*) begin
+        if(rst == 1'b1) begin
+            mem_w_data = `ZERO_WORD;
+            mem_w_ena = 1'b0;
+            mem_w_addr = `ZERO_REG_ADDR;
+            mem_mem_waddr = `ZERO_WORD;
+            mem_mem_raddr = `ZERO_WORD;
+            mem_sel = 8'b0000_0000;
+            mem_stor_data = `ZERO_WORD;
+            mem_wr = 1'b0;
+            mem_mem_ena = 1'b0;
+        end
+        else begin
+            mem_w_data = ex_w_data;
+            mem_w_ena = ex_w_ena;
+            mem_w_addr = ex_w_addr;
+            mem_mem_waddr = ex_mem_waddr;
+            mem_mem_raddr = ex_mem_raddr;
+            mem_stor_data = ex_stor_data;
+            mem_sel = 8'b0000_0000;
+            mem_wr = ex_mem_wr;
+            mem_mem_ena = ex_mem_ena;
+            case(ex_memop)
+                 `R_ONE:begin
+                     mem_sel = 8'b0000_0001;
+                     mem_w_data = {{56{mem_data[7]}} , mem_data[7 : 0]};
+                 end
+                 `R_ONEu:begin
+                     mem_sel = 8'b0000_0001;
+                     mem_w_data = {{56{1'b0}} , mem_data[7 : 0]};
+                 end
+                 `R_DOU:begin
+                     mem_sel = 8'b0000_0011;
+                     mem_w_data = {{48{mem_data[15]}} , mem_data[15 : 0]};
+                 end
+                 `R_DOUu:begin
+                     mem_sel = 8'b0000_0011;
+                     mem_w_data = {{48{1'b0}} , mem_data[15 : 0]};
+                 end
+                 `R_FOR:begin
+                     mem_sel = 8'b0000_1111;
+                     mem_w_data = {{32{mem_data[31]}} , mem_data[31 : 0]};
+                 end
+                 `R_FORu:begin
+                     mem_sel = 8'b0000_1111;
+                     mem_w_data = {{32{1'b0}} , mem_data[31 : 0]};
+                 end
+                 `R_EIG:begin
+                     mem_sel = 8'b1111_1111;
+                     mem_w_data = mem_data;
+                 end
+                 `W_ONE:begin
+                     mem_sel = 8'b0000_0001;
+                     mem_mem_ena = 1'b1;
+                 end
+                 `W_DOU:begin
+                     mem_sel = 8'b0000_0011;
+                     mem_mem_ena = 1'b1;
+                 end
+                 `W_FOR:begin
+                     mem_sel = 8'b0000_1111;
+                     mem_mem_ena = 1'b1;
+                 end
+                 `W_EIG:begin
+                     mem_sel = 8'b1111_1111;
+                     mem_mem_ena = 1'b1;
+                 end
+                 default:begin
+                     mem_w_data = ex_w_data;
+                     mem_sel = 8'b0000_0000;
+                     mem_wr = 1'b0;
+                     mem_mem_ena = 1'b0;
+                 end
+            endcase
+        end
+    end
+endmodule
