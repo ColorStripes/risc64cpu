ffaa41bb5a61e9f2040f672ee4237ac5a453e5d1
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-06 00:04:00 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 770b95c4b0544c3da8f09e5429ab96ae     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  00:04:00 up 11:24,  1 user,  load average: 0.79, 0.45, 0.32

--- projects/cpu_diff/vsrc/IMGN.v
+++ projects/cpu_diff/vsrc/IMGN.v
@@ -28,15 +28,15 @@ module IMGN (
              end
              5'b00100://B-type
              begin
-                 imm = {{52{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8]};
+                 imm = {{51{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
              end
              5'b00010://J-type
              begin
-                 imm = {{44{instr[31]}}, instr[31], instr[19 : 12], instr[20], instr[30 : 21]};
+                 imm = {{43{instr[31]}}, instr[31], instr[19 : 12], instr[20], instr[30 : 21], 1'b0};
              end
              5'b00001://U_type
              begin
-                 imm = {{44{instr[31]}}, instr[31 : 12]};
+                 imm = {{32{instr[31]}}, instr[31 : 12], 12'h000};
              end
              default:
              begin
