ee1ce2268b46cb86ee18fb35457cfad644bab2b3
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-05 16:31:24 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 770b95c4b0544c3da8f09e5429ab96ae     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  16:31:24 up  8:17,  1 user,  load average: 0.30, 0.24, 0.26

--- projects/cpu_diff/vsrc/SimTop.v
+++ projects/cpu_diff/vsrc/SimTop.v
@@ -19,386 +19,90 @@ module SimTop(
     input  [7:0]  io_uart_in_ch
 );
 
-//SIMTOP -> rvcpu
-wire clk;
-wire rst;
+// if_stage
+wire [63 : 0] pc;
+wire [31 : 0] inst;
 
-//IF_stage -> if_id
-wire wash;
-wire [`PC_BUS] pc;
-wire [31 : 0] instr;
+// id_stage
+// id_stage -> regfile
+wire rs1_r_ena;
+wire [4 : 0]rs1_r_addr;
+wire rs2_r_ena;
+wire [4 : 0]rs2_r_addr;
+wire rd_w_ena;
+wire [4 : 0]rd_w_addr;
+// id_stage -> exe_stage
+wire [4 : 0]inst_type;
+wire [7 : 0]inst_opcode;
+wire [`REG_BUS]op1;
+wire [`REG_BUS]op2;
 
-//if_id -> ID_stage
-wire [`INST_BUS] id_instr;
-//if_id -> IF_stage too
-wire [`PC_BUS] id_pc;
-
-//regfile -> ID_stage
+// regfile -> id_stage
 wire [`REG_BUS] r_data1;
 wire [`REG_BUS] r_data2;
-
 // regfile -> difftest
 wire [`REG_BUS] regs[0 : 31];
 
-//ID_stage -> regfile
-wire reg1_r_ena;
-wire reg2_r_ena;
-wire [4 : 0] reg1_addr;
-wire [4 : 0] reg2_addr;
-
-//ID_stage -> IF_stage
-wire [`PC_BUS] branch;
-wire pc_con;
-wire mux_pc;
-
-//ID_stage -> id_ex
-wire [`PC_BUS] ID_pc; 
-wire [6 : 0] aluop;
-wire [2 : 0] alusel;
-wire [`REG_BUS] reg1_data;
-wire [`REG_BUS] reg2_data;
-wire [4 : 0] w_addr;
-wire w_ena;
-wire [4 : 0] memop;
-wire [63 : 0] imm;
-wire id_mem_wr;
-wire id_mem_ena;
+// exe_stage
+// exe_stage -> other stage
+wire [4 : 0]inst_type_o;
+// exe_stage -> regfile
+wire [`REG_BUS]rd_data;
 
 
-//id_ex -> EX_stage
-wire [4 : 0] ex_w_addr;
-wire ex_w_ena;
-wire [`REG_BUS] ex_reg1_data;
-wire [`REG_BUS] ex_reg2_data;
-wire [6 : 0] ex_aluop;
-wire [2 : 0] ex_alusel;
-wire [`PC_BUS] ex_pc;
-wire [4 : 0] ex_memop;
-wire [63 : 0] ex_imm;
-//id_ex -> ID_stage too
-wire ex_mem_wr;
-wire ex_mem_ena;
-
-
-//EX_stage -> ex_mem
-wire [`REG_BUS] ex_w_data;
-wire EX_w_ena;
-wire [4 : 0] EX_w_addr;
-wire [`PC_BUS] EX_pc;
-wire [4 : 0] EX_memop;
-wire [`REG_BUS] ex_mem_waddr;
-wire [`REG_BUS] ex_mem_raddr;
-wire [`REG_BUS] ex_stor_data;
-wire EX_mem_wr;
-wire EX_mem_ena;
-
-//ex_men -> MEM_stage
-wire [`REG_BUS] mem_w_data;
-wire mem_w_ena;
-wire [4 : 0] mem_w_addr;
-wire [`PC_BUS] men_pc;
-wire [`REG_BUS] mem_mem_waddr;
-wire [`REG_BUS] mem_mem_raddr;
-wire [4 : 0] mem_memop;
-wire [`REG_BUS] mem_stor_data;
-wire mem_mem_wr;
-wire mem_mem_ena;
-
-//MEM_stage -> mem_wb
-wire [`REG_BUS] MEM_w_data;
-wire MEM_w_ena;
-wire [4 : 0] MEM_w_addr;
-
-//MEM_stage -> DATA_MEM
-wire [`REG_BUS] MEM_mem_waddr;
-wire [`REG_BUS] MEM_mem_raddr;
-wire [7 : 0] mem_sel;
-wire [`REG_BUS] MEM_stor_data;
-wire mem_wr;
-wire MEM_mem_ena;
-
-//DATA_MEM -> MEM_stage
-wire [63 : 0] data;
-
-//mem_wb -> WB_stage
-wire [`REG_BUS] wb_w_data;
-wire wb_w_ena;
-wire [4 : 0] wb_w_addr;
-
-//WB_stage -> regfile
-wire WB_w_ena;
-wire [`REG_BUS] WB_w_data;
-wire [4 : 0] WB_w_addr;
-
-//radehelper ->instr
-
-
-assign clk = clock;
-assign rst = reset;
-
-    IF_stage IF_stage (
-    .rst(rst),
-    .clk(clk),
-    .branch(branch),
-    .mux_pc(mux_pc),
-    .pc_con(pc_con),
-    .pc_id(id_pc),
-
-    .wash(wash),
-    .IF_pc(pc),
-    .instr(instr)
-);
-
-
-
-    if_id if_id (
-    .rst(rst),
-    .clk(clk),
-    .if_pc(pc),
-    .if_instr(instr),
-    .pc_con(pc_con),
-    .wash(wash),
-
-    .id_pc(id_pc),
-    .id_instr(id_instr)
-);
-
-    regfile regfile(
-    .clk(clk),
-	  .rst(rst),
-	
-	  .w_addr(WB_w_addr),
-	  .w_data(WB_w_data),
-	  .w_ena(WB_w_ena),
-	
-  	.r_addr1(reg1_addr),
-	  .r_ena1(reg1_r_ena),
-  	.r_data1(r_data1),  //OUT1
-
-	  .r_addr2(reg2_addr),
-	  .r_ena2(reg2_r_ena),
-	  .r_data2(r_data2),  //OUT2
-    .regs_o(regs)
+if_stage If_stage(
+  .clk                (clock),
+  .rst                (reset),
   
-
+  .pc                 (pc),
+  .inst               (inst)
 );
 
-    ID_stage ID_stage (
-    .rst(rst),
-    .IF_pc(id_pc), 
-    .IF_instr(id_instr),
-
-    .reg_data1(r_data1), //
-    .reg_data2(r_data2), //
-
-    .reg1_r_ena(reg1_r_ena),
-    .reg2_r_ena(reg2_r_ena),
-
-    .reg1_addr(reg1_addr),
-    .reg2_addr(reg2_addr),
-
-    .ex_w_data(ex_w_data),    //ex_stage for data
-    .ex_w_ena(ex_w_ena),
-    .ex_w_addr(ex_w_addr),
-
-    .idex_mem_ena(ex_mem_ena),          //id_ex memory enable
-    .idex_mem_wr(ex_mem_wr),
-
-
-    .mem_w_data(MEM_w_data),   //men_stage for data
-    .mem_w_ena(MEM_w_ena),
-    .mem_w_addr(MEM_w_addr),
-
-    .aluop(aluop),          //ALUoptions
-    .alusel(alusel),
-
-    .reg1_data(reg1_data),  //
-    .reg2_data(reg2_data),  //
-
-    .w_addr(w_addr),       
-    .w_ena(w_ena),                  //write enable
-
-    .ID_pc(ID_pc),       //pc now
-    .branch(branch),    //pc next
-    .mux_pc(mux_pc),
-    .pc_con(pc_con),
-    .imm(imm),
-
-    .memop(memop),
-    .id_mem_wr(id_mem_wr),
-    .id_mem_ena(id_mem_ena)
-
+id_stage Id_stage(
+  .rst                (reset),
+  .inst               (inst),
+  .rs1_data           (r_data1),
+  .rs2_data           (r_data2),
+  
+  .rs1_r_ena          (rs1_r_ena),
+  .rs1_r_addr         (rs1_r_addr),
+  .rs2_r_ena          (rs2_r_ena),
+  .rs2_r_addr         (rs2_r_addr),
+  .rd_w_ena           (rd_w_ena),
+  .rd_w_addr          (rd_w_addr),
+  .inst_type          (inst_type),
+  .inst_opcode        (inst_opcode),
+  .op1                (op1),
+  .op2                (op2)
 );
 
-    id_ex id_ex (
-    .rst(rst),
-    .clk(clk),
-
-    .id_pc(ID_pc),
-
-    .id_aluop(aluop),
-    .id_alusel(alusel),
-    .id_imm(imm),
-
-    .id_reg1_data(reg1_data),
-    .id_reg2_data(reg2_data),
-
-    .id_w_ena(w_ena),
-    .id_w_addr(w_addr),
-
-    .id_memop(memop),
-    .id_mem_wr(id_mem_wr),
-    .id_mem_ena(id_mem_ena),
-
-    .ex_w_addr(ex_w_addr),
-    .ex_w_ena(ex_w_ena),
-
-    .ex_reg1_data(ex_reg1_data),
-    .ex_reg2_data(ex_reg2_data),
-
-    .ex_memop(ex_memop),
-    .ex_aluop(ex_aluop),
-    .ex_alusel(ex_alusel),
-    .ex_imm(ex_imm),
-    .ex_mem_wr(ex_mem_wr),
-    .ex_mem_ena(ex_mem_ena),
-
-    .ex_pc(ex_pc)
-    
-); 
-
-    EX_stage EX_stage (
-    .rst(rst),
-
-    .ID_pc(ex_pc),//未写
-
-    .id_w_addr(ex_w_addr),
-    .id_w_ena(ex_w_ena),
-
-    .id_reg1_data(ex_reg1_data),
-    .id_reg2_data(ex_reg2_data),
-
-    .id_aluop(ex_aluop),
-    .id_alusel(ex_alusel),
-    .id_memop(ex_memop),
-    .id_imm(ex_imm),
-    .id_mem_wr(ex_mem_wr),
-    .id_mem_ena(ex_mem_ena),
-
-    .ex_w_data(ex_w_data),
-    .ex_w_ena(EX_w_ena),
-    .ex_w_addr(EX_w_addr),
-
-    .ex_memop(EX_memop),
-    .ex_mem_waddr(ex_mem_waddr),
-    .ex_mem_raddr(ex_mem_raddr),
-    .ex_stor_data(ex_stor_data),
-    .ex_mem_wr(EX_mem_wr),
-    .ex_mem_ena(EX_mem_ena),
-
-    .EX_pc(EX_pc) //未写
+exe_stage Exe_stage(
+  .rst                (reset),
+  .inst_type_i        (inst_type),
+  .inst_opcode        (inst_opcode),
+  .op1                (op1),
+  .op2                (op2),
+  
+  .inst_type_o        (inst_type_o),
+  .rd_data            (rd_data)
 );
 
-    ex_mem ex_mem (
-    .rst(rst),
-    .clk(clk),
-    .ex_pc(EX_pc),
-    .ex_w_data(ex_w_data),
-    .ex_w_ena(EX_w_ena),
-    .ex_w_addr(EX_w_addr),
-    .ex_mem_waddr(ex_mem_waddr),
-    .ex_mem_raddr(ex_mem_raddr),
-    .ex_memop(EX_memop),
-    .ex_stor_data(ex_stor_data),
-    .ex_mem_wr(EX_mem_wr),
-    .ex_mem_ena(EX_mem_ena),
+regfile Regfile(
+  .clk                (clock),
+  .rst                (reset),
+  .w_addr             (rd_w_addr),
+  .w_data             (rd_data),
+  .w_ena              (rd_w_ena),
+  
+  .r_addr1            (rs1_r_addr),
+  .r_data1            (r_data1),
+  .r_ena1             (rs1_r_ena),
+  .r_addr2            (rs2_r_addr),
+  .r_data2            (r_data2),
+  .r_ena2             (rs2_r_ena),
 
-    .mem_w_data(mem_w_data),
-    .mem_w_ena(mem_w_ena),
-    .mem_w_addr(mem_w_addr),
-
-    .mem_mem_waddr(mem_mem_waddr),
-    .mem_mem_raddr(mem_mem_raddr),
-    .mem_memop(mem_memop),
-    .mem_stor_data(mem_stor_data),
-    .mem_mem_wr(mem_mem_wr),
-    .mem_mem_ena(mem_mem_ena),
-
-    .men_pc() //未连
+  .regs_o             (regs)
 );
-
-    MEM_stage MEM_stage (
-    .rst(rst),
-    .ex_w_data(mem_w_data),
-    .ex_w_ena(mem_w_ena),
-    .ex_w_addr(mem_w_addr),
-    .ex_mem_waddr(mem_mem_waddr),
-    .ex_mem_raddr(mem_mem_raddr),
-    .ex_memop(mem_memop),
-    .mem_data(data),
-    .ex_stor_data(mem_stor_data),
-    .ex_mem_wr(mem_mem_wr),
-    .ex_mem_ena(mem_mem_ena),
-
-    .mem_w_data(MEM_w_data),
-    .mem_w_ena(MEM_w_ena),
-    .mem_w_addr(MEM_w_addr),
-
-    .mem_mem_waddr(MEM_mem_waddr),
-    .mem_mem_raddr(MEM_mem_raddr),
-    .mem_sel(mem_sel),
-    .mem_stor_data(MEM_stor_data),
-    .mem_wr(mem_wr),
-    .mem_mem_ena(MEM_mem_ena)
-);
-
-
-    RAMHelper RAMHelper(
-    .clk(clk),
-    .en(MEM_mem_ena),
-    .rIdx({3'b000,(MEM_mem_raddr-64'h0000_0000_8000_0000)>>3}),
-    .rdata(data),
-    .wIdx({3'b000,(MEM_mem_waddr-64'h0000_0000_8000_0000)>>3}),
-    .wdata(MEM_stor_data),
-    .wmask(0),          //!!!!!!!!!!!
-    .wen(mem_wr)
-);
-
-    mem_wb mem_wb (
-    .clk(clk),
-    .rst(rst),
-    .mem_w_data(MEM_w_data),
-    .mem_w_ena(MEM_w_ena),
-    .mem_w_addr(MEM_w_addr),
-
-    .wb_w_data(wb_w_data),
-    .wb_w_ena(wb_w_ena),
-    .wb_w_addr(wb_w_addr)
-);
-
-   WB_stage WB_stage (
-    .rst(rst),
-    .mem_w_ena(wb_w_ena),
-    .mem_w_data(wb_w_data),
-    .mem_w_addr(wb_w_addr),
-
-    .wb_w_ena(WB_w_ena),
-    .wb_w_data(WB_w_data),
-    .wb_w_addr(WB_w_addr)
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Difftest
@@ -414,23 +118,23 @@ reg [63:0] cycleCnt;
 reg [63:0] instrCnt;
 reg [`REG_BUS] regs_diff [0 : 31];
 
-wire inst_valid = (id_pc != `PC_START) | (id_instr != 0);
+wire inst_valid = (pc != `PC_START) | (inst != 0);
 
 always @(negedge clock) begin
   if (reset) begin
     {cmt_wen, cmt_wdest, cmt_wdata, cmt_pc, cmt_inst, cmt_valid, trap, trap_code, cycleCnt, instrCnt} <= 0;
   end
   else if (~trap) begin
-    cmt_wen <= WB_w_ena;//
-    cmt_wdest <= {3'd0, WB_w_addr};//
-    cmt_wdata <= WB_w_data;//
-    cmt_pc <= pc;//
-    cmt_inst <= instr;//
+    cmt_wen <= rd_w_ena;
+    cmt_wdest <= {3'd0, rd_w_addr};
+    cmt_wdata <= rd_data;
+    cmt_pc <= pc;
+    cmt_inst <= inst;
     cmt_valid <= inst_valid;
 
 		regs_diff <= regs;
 
-    trap <= id_instr[6:0] == 7'h6b;
+    trap <= inst[6:0] == 7'h6b;
     trap_code <= regs[10][7:0];
     cycleCnt <= cycleCnt + 1;
     instrCnt <= instrCnt + inst_valid;
@@ -558,7 +262,5 @@ DifftestArchFpRegState DifftestArchFpRegState(
   .fpr_30             (0),
   .fpr_31             (0)
 );
-
-
 
 endmodule
