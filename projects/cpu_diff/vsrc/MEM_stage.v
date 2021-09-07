0c3bb3f57ffcb3d94d206809cf806d01cf369740
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-07 16:21:23 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 770b95c4b0544c3da8f09e5429ab96ae     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  16:21:24 up 17:56,  1 user,  load average: 0.22, 0.11, 0.14

--- projects/cpu_diff/vsrc/MEM_stage.v
+++ projects/cpu_diff/vsrc/MEM_stage.v
@@ -61,7 +61,7 @@ module MEM_stage (
             mem_mem_ena = ex_mem_ena;
             case(ex_memop)
                  `R_ONE:begin
-                     mem_sel = 64'h0000_0000_0000_00ff;
+                     mem_sel = 64'hff0000_0000_0000_00;
                      mem_w_data = {{56{mem_data[7]}} , mem_data[7 : 0]};
                  end
                  `R_ONEu:begin
