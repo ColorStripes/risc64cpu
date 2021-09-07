41b55050262d4236ec5ddc6a77bce7c7edcffa20
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-07 16:03:42 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 770b95c4b0544c3da8f09e5429ab96ae     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  16:03:42 up 17:38,  1 user,  load average: 0.81, 0.38, 0.23

--- projects/cpu_diff/vsrc/SimTop.v
+++ projects/cpu_diff/vsrc/SimTop.v
@@ -381,7 +381,7 @@ assign rst = reset;
     .rdata(data),
     .wIdx({3'b000,(MEM_mem_waddr-64'h0000_0000_8000_0000)>>3}),
     .wdata(MEM_stor_data),
-    .wmask(64'hffff_ffff_ffff_ffff),          //!!!!!!!!!!!
+    .wmask(mem_sel),          //!!!!!!!!!!!
     .wen(mem_wr)
 );
 
