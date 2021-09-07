4693451e30a8eb2fc6839c18deeb2399817b0d8d
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-07 18:10:53 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 770b95c4b0544c3da8f09e5429ab96ae     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  18:10:54 up 19:45,  1 user,  load average: 0.48, 0.43, 0.34

--- projects/cpu_diff/vsrc/MEM_stage.v
+++ projects/cpu_diff/vsrc/MEM_stage.v
@@ -61,44 +61,203 @@ module MEM_stage (
             mem_mem_ena = ex_mem_ena;
             case(ex_memop)
                  `R_ONE:begin
-                     mem_sel = 64'h0000_0000_0000_00ff;
-                     mem_w_data = {{56{mem_data[63]}} , mem_data[63 : 56]};
+                     case(ex_mem_raddr[2 : 0])
+                         3'b000:begin
+                             mem_sel = 64'h0000_0000_0000_00ff;
+                             mem_w_data = {{56{mem_data[7]}} , mem_data[7 : 0]};
+                         end
+                         3'b001:begin
+                             mem_sel = 64'h0000_0000_0000_ff00;
+                             mem_w_data = {{56{mem_data[15]}} , mem_data[15 : 8]};
+                         end
+                         3'b010:begin
+                             mem_sel = 64'h0000_0000_00ff_0000;
+                             mem_w_data = {{56{mem_data[23]}} , mem_data[23 : 16]};
+                         end
+                         3'b011:begin
+                             mem_sel = 64'h0000_0000_ff00_0000;
+                             mem_w_data = {{56{mem_data[31]}} , mem_data[31 : 24]};
+                         end
+                         3'b100:begin
+                             mem_sel = 64'h0000_00ff_0000_0000;
+                             mem_w_data = {{56{mem_data[39]}} , mem_data[39 : 32]};
+                         end
+                         3'b101:begin
+                             mem_sel = 64'h0000_ff00_0000_0000;
+                             mem_w_data = {{56{mem_data[48]}} , mem_data[47 : 40]};
+                         end
+                         3'b110:begin
+                             mem_sel = 64'h00ff_0000_0000_0000;
+                             mem_w_data = {{56{mem_data[55]}} , mem_data[55 : 48]};
+                         end
+                         3'b111:begin
+                             mem_sel = 64'hff00_0000_0000_0000;
+                             mem_w_data = {{56{mem_data[63]}} , mem_data[63 : 56]};
+                         end
+                     endcase 
                  end
-                 `R_ONEu:begin
-                     mem_sel = 64'h0000_0000_0000_00ff;
-                     mem_w_data = {{56{1'b0}} , mem_data[63 : 56]};
+                 `R_ONEu:begin                    
+                     case(ex_mem_raddr[2 : 0])
+                         3'b000:begin
+                             mem_sel = 64'h0000_0000_0000_00ff;
+                             mem_w_data = {{56{1'b0}} , mem_data[7 : 0]};
+                         end
+                         3'b001:begin
+                             mem_sel = 64'h0000_0000_0000_ff00;
+                             mem_w_data = {{56{1'b0}} , mem_data[15 : 8]};
+                         end
+                         3'b010:begin
+                             mem_sel = 64'h0000_0000_00ff_0000;
+                             mem_w_data = {{56{1'b0}} , mem_data[23 : 16]};
+                         end
+                         3'b011:begin
+                             mem_sel = 64'h0000_0000_ff00_0000;
+                             mem_w_data = {{56{1'b0}} , mem_data[31 : 24]};
+                         end
+                         3'b100:begin
+                             mem_sel = 64'h0000_00ff_0000_0000;
+                             mem_w_data = {{56{1'b0}} , mem_data[39 : 32]};
+                         end
+                         3'b101:begin
+                             mem_sel = 64'h0000_ff00_0000_0000;
+                             mem_w_data = {{56{1'b0}} , mem_data[47 : 40]};
+                         end
+                         3'b110:begin
+                             mem_sel = 64'h00ff_0000_0000_0000;
+                             mem_w_data = {{56{1'b0}} , mem_data[55 : 48]};
+                         end
+                         3'b111:begin
+                             mem_sel = 64'hff00_0000_0000_0000;
+                             mem_w_data = {{56{1'b0}} , mem_data[63 : 56]};
+                         end
+                     endcase  
                  end
                  `R_DOU:begin
-                     mem_sel = 64'h0000_0000_0000_ffff;
-                     mem_w_data = {{48{mem_data[63]}} , mem_data[63 : 48]};
+                     case(ex_mem_raddr[2 : 1])
+                         2'b00:begin
+                             mem_sel = 64'h0000_0000_0000_ffff;
+                             mem_w_data = {{48{mem_data[15]}} , mem_data[15 : 0]};
+                         end
+                         2'b01:begin
+                             mem_sel = 64'h0000_0000_ffff_0000;
+                             mem_w_data = {{48{mem_data[31]}} , mem_data[31 : 16]};
+                         end
+                         2'b10:begin
+                             mem_sel = 64'h0000_ffff_0000_0000;
+                             mem_w_data = {{48{mem_data[47]}} , mem_data[47 : 32]};
+                         end
+                         2'b11:begin
+                             mem_sel = 64'hffff_0000_0000_0000;
+                             mem_w_data = {{48{mem_data[63]}} , mem_data[63 : 48]};
+                         end 
+                     endcase  
                  end
                  `R_DOUu:begin
-                     mem_sel = 64'h0000_0000_0000_ffff;
-                     mem_w_data = {{48{1'b0}} , mem_data[63 : 48]};
+                     case(ex_mem_raddr[2 : 1])
+                         2'b00:begin
+                             mem_sel = 64'h0000_0000_0000_ffff;
+                             mem_w_data = {{48{1'b0}} , mem_data[15 : 0]};
+                         end
+                         2'b01:begin
+                             mem_sel = 64'h0000_0000_ffff_0000;
+                             mem_w_data = {{48{1'b0}} , mem_data[31 : 16]};
+                         end
+                         2'b10:begin
+                             mem_sel = 64'h0000_ffff_0000_0000;
+                             mem_w_data = {{48{1'b0}} , mem_data[47 : 32]};
+                         end
+                         2'b11:begin
+                             mem_sel = 64'hffff_0000_0000_0000;
+                             mem_w_data = {{48{1'b0}} , mem_data[63 : 48]};
+                         end 
+                     endcase
                  end
                  `R_FOR:begin
-                     mem_sel = 64'h0000_0000_ffff_ffff;
-                     mem_w_data = {{32{mem_data[31]}} , mem_data[31 : 0]};
+                     case(ex_mem_raddr[2])
+                         1'b0:begin
+                             mem_sel = 64'h0000_0000_ffff_ffff;
+                             mem_w_data = {{32{mem_data[31]}} , mem_data[31 : 0]};
+                         end
+                         1'b1:begin
+                             mem_sel = 64'hffff_ffff_0000_0000;
+                             mem_w_data = {{32{mem_data[63]}} , mem_data[63 : 32]};
+                         end
+                     endcase
                  end
                  `R_FORu:begin
-                     mem_sel = 64'h0000_0000_ffff_ffff;
-                     mem_w_data = {{32{1'b0}} , mem_data[31 : 0]};
+                     case(ex_mem_raddr[2])
+                         1'b0:begin
+                             mem_sel = 64'h0000_0000_ffff_ffff;
+                             mem_w_data = {{32{1'b0}} , mem_data[31 : 0]};
+                         end
+                         1'b1:begin
+                             mem_sel = 64'hffff_ffff_0000_0000;
+                             mem_w_data = {{32{1'b0}} , mem_data[63 : 32]};
+                         end
+                     endcase
                  end
                  `R_EIG:begin
                      mem_sel = 64'hffff_ffff_ffff_ffff;
                      mem_w_data = mem_data;
                  end
+
+                 //write
                  `W_ONE:begin
-                     mem_sel = 64'h0000_0000_0000_00ff;
                      mem_mem_ena = 1'b1;
+                     case(ex_mem_waddr[2 : 0])
+                         3'b000:begin
+                             mem_sel = 64'h0000_0000_0000_00ff;
+                         end
+                         3'b001:begin
+                             mem_sel = 64'h0000_0000_0000_ff00;
+                         end
+                         3'b010:begin
+                             mem_sel = 64'h0000_0000_00ff_0000;
+                         end
+                         3'b011:begin
+                             mem_sel = 64'h0000_0000_ff00_0000;
+                         end
+                         3'b100:begin
+                             mem_sel = 64'h0000_00ff_0000_0000;
+                         end
+                         3'b101:begin
+                             mem_sel = 64'h0000_ff00_0000_0000;
+                         end
+                         3'b110:begin
+                             mem_sel = 64'h00ff_0000_0000_0000;
+                         end
+                         3'b111:begin
+                             mem_sel = 64'hff00_0000_0000_0000;
+                         end
+                     endcase  
                  end
                  `W_DOU:begin
-                     mem_sel = 64'h0000_0000_0000_ffff;
                      mem_mem_ena = 1'b1;
+                     case(ex_mem_raddr[2 : 1])
+                         2'b00:begin
+                             mem_sel = 64'h0000_0000_0000_ffff;
+                         end
+                         2'b01:begin
+                             mem_sel = 64'h0000_0000_ffff_0000;
+                         end
+                         2'b10:begin
+                             mem_sel = 64'h0000_ffff_0000_0000;
+                         end
+                         2'b11:begin
+                             mem_sel = 64'hffff_0000_0000_0000;
+                         end 
+                     endcase
                  end
                  `W_FOR:begin
-                     mem_sel = 64'h0000_0000_ffff_ffff;
                      mem_mem_ena = 1'b1;
+                     case(ex_mem_raddr[2])
+                         1'b0:begin
+                             mem_sel = 64'h0000_0000_ffff_ffff;
+                         end
+                         1'b1:begin
+                             mem_sel = 64'hffff_ffff_0000_0000;
+                         end
+                     endcase
                  end
                  `W_EIG:begin
                      mem_sel = 64'hffff_ffff_ffff_ffff;
