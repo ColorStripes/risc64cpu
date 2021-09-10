bf976866a2a06ec551e87714f56318546f3e5c7f
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-11 03:05:47 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 27d64cd4f3b7452baba4a562ac71d12a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  03:05:47 up 18:43,  1 user,  load average: 0.31, 0.47, 0.52

--- projects/cpu_diff/vsrc/forecase.v
+++ projects/cpu_diff/vsrc/forecase.v
@@ -19,35 +19,73 @@ module forecase (
     reg [1 : 0] fore;
     reg [`PC_BUS] fore_branch[`FORECASE-1 : 0];
     reg [`PC_BUS] pc_now[3 : 0];
+
+    reg [1 : 0] fore_reg;
+    reg [`PC_BUS] fore_branch_reg[`FORECASE-1 : 0];
+    reg [`PC_BUS] pc_now_reg[3 : 0];
     reg if_forecase;
 
+
+    reg test;
+    reg test1;
+    reg [63 :0] pc_s;
+    reg [63 :0] add;
+
 always @(posedge clk) begin
+    if(rst == 0)
+ add <= 64'h4;
+ else
+     add <= add_pc;
+end
+
+
+
+
+always @(*) begin
     if(rst == 1'b1) begin
+        test = 1'b0;
+        //pp = 64'b0;
+        //p = 64'b0;
         fore = 2'b00;
-        for(i=0; i<4; i=i+1) begin
-            pc_now[i] <= `ZERO_WORD; 
+        pc_s = `ZERO_WORD;
+        for(i=0; i<`PC; i=i+1) begin
+            pc_now[i] = `ZERO_WORD; 
         end
         for(i=0; i<`FORECASE; i=i+1) begin
-            fore_branch[i] <= `ZERO_WORD; 
+            fore_branch[i] = `ZERO_WORD; 
         end
     end
     else begin
+        fore = fore_reg;
+        pc_now = pc_now_reg;
+        fore_branch = fore_branch_reg;
+        test = 1'b0;
+        pc_s = `ZERO_WORD;
+        //pp = pc_id;
+                //p = pc_now[add[3 : 2]];
+                //test = test;
+                if(pc_now[add[3 : 2]]  == add )  begin
+                    test = 1'b1;
+                end
+
+
         if(pc_con != 1'b1) begin
             if(mux_pc == 1'b1) begin
-                if(fore_branch[pc_id[`FORECASE_LOG+1 : 2]] != branch) begin
-                    fore_branch[pc_id[`FORECASE_LOG+1 : 2]] = branch; 
-                    pc_now[pc_id[3 : 2]] = pc_id;
+                pc_s = pc_id + 4;
+                if(fore_branch[pc_s[`FORECASE_LOG+1 : 2]] != branch) begin
+                    fore_branch[pc_s[`FORECASE_LOG+1 : 2]] = branch; 
+                    pc_now[pc_s[`PC_LOG+1 : 2]] = pc_id + 4;
                 end
+                if(fore < 2'b11) begin
+                    fore = fore + 1;
+                end
+
             end
-            if(add_pc == pc_now[add_pc[3 : 2]] + 4) begin
-                if(mux_pc >= 1'b1) begin
-                    if(fore < 2'b11) begin
-                        fore = fore + 1;
-                    end
-                end
-                else begin
+            
+            else begin
+                if(add == pc_now[add[`PC_LOG+1 : 2]]) begin
                     if(fore > 2'b00) begin
-                    fore = fore - 1;
+                        fore = fore - 1;
                     end
                 end
             end
@@ -55,8 +93,11 @@ always @(posedge clk) begin
     end
 end
 
-
-
+always @(posedge clk) begin
+        fore_reg = fore;
+        pc_now_reg = pc_now;
+        fore_branch_reg = fore_branch;
+end
 
     always @(*) begin
         if(rst == 1'b1) begin
@@ -70,6 +111,10 @@ end
             if_forecase = 1'b0;
             if(pc_con != 1'b1) begin
                 
+                   
+
+
+
                 if(mux_pc != if_forecase) begin
                     wash = 1'b1;
                     pc = branch;
@@ -78,10 +123,11 @@ end
                     wash = 1'b0;
                     pc = add_pc;
                 end
+                
 
-                if(add_pc == pc_now[add_pc[3 : 2]] + 4) begin
+                if(add == pc_now[add[`PC_LOG + 1 : 2]]) begin
                     if(fore >= 2'b10) begin
-                        pc = fore_branch[add_pc[`FORECASE_LOG+1 : 2]];
+                        pc = fore_branch[add[`FORECASE_LOG + 1 : 2]];
                         if_forecase = 1'b1;
                     end
                     else begin
