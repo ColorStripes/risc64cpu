6afc7a9c53895144554c998b5a5fd1217f8e73f5
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-11 15:36:25 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 27d64cd4f3b7452baba4a562ac71d12a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  15:36:25 up 23:45,  1 user,  load average: 0.46, 0.28, 0.20

--- projects/cpu_diff/vsrc/forecase.v
+++ projects/cpu_diff/vsrc/forecase.v
@@ -100,7 +100,7 @@ reg test;
             wash = 1'b0;
             pc = `ZERO_WORD;
             if_forecase = 1'b0;
-            //if(pc_con != 1'b1) begin
+            if(pc_con != 1'b1) begin
                 if(add_pc == pc_now[add_pc[`PC_LOG + 1 : 2]]) begin
                     if(fore >= 2'b10) begin
                         pc = fore_branch[add_pc[`FORECASE_LOG + 1 : 2]];
@@ -134,7 +134,7 @@ reg test;
                         pc = pc_id + 4;
                     end
                 end
-            //end
+            end
         end
     end
 
