ba3a1b4f9b92d3e2272c256a18c96ad1c5cb8f8c
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-11 15:10:18 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 27d64cd4f3b7452baba4a562ac71d12a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  15:10:18 up 23:18,  1 user,  load average: 0.51, 0.33, 0.21

--- projects/cpu_diff/vsrc/forecase.v
+++ projects/cpu_diff/vsrc/forecase.v
@@ -30,6 +30,7 @@ module forecase (
     reg error_branch;  //if branch != forecase_branch when mux_pc==1
 
 always @(posedge clk) begin
+    if(pc_con == 1'b0)
         ifa <= if_forecase;
 end
 
@@ -53,7 +54,7 @@ always @(*) begin
         pc_s = `ZERO_WORD;
         error_branch = 1'b0;
 
-        //if(pc_con != 1'b1) begin
+        if(pc_con != 1'b1) begin
             if(mux_pc == 1'b1) begin
                 pc_s = pc_id + 4;
                 if(fore_branch[pc_s[`FORECASE_LOG+1 : 2]] != branch) begin
@@ -73,7 +74,7 @@ always @(*) begin
                     end
                 end
             end
-        //end
+        end
     end
 end
 
@@ -132,18 +133,6 @@ reg test;
                     end
                 end
             end
-                if(mux_pc == 1'b1) begin
-                    if((mux_pc != ifa) || (error_branch)) begin  
-                       wash = 1'b1;
-                       pc = branch;
-                    end
-                end
-                else begin
-                    if(mux_pc != ifa) begin
-                        wash = 1'b1;
-                        pc = pc_id + 4;
-                    end
-                end
         end
     end
 
