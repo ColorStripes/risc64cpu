e512777dfaf0cf913a68217b16ce558795d50938
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-11 23:13:52 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 27d64cd4f3b7452baba4a562ac71d12a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  23:13:52 up 1 day,  5:45,  1 user,  load average: 0.47, 0.38, 0.29

--- projects/cpu_diff/vsrc/EX_stage.v
+++ projects/cpu_diff/vsrc/EX_stage.v
@@ -101,7 +101,7 @@ ALU ALU(
                       ex_w_data = {{32{result[31]}}, result[31 : 0]};
                   end
                   `No:begin
-                      $fwrite("%s",result);
+                      $fwrite("%d",result);
                   end
                   default: begin
                       ex_w_data = `ZERO_WORD;
