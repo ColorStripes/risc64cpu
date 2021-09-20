c77da876f8441d35e0b04ba803117a41a760cac8
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-20 20:05:20 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 6d388c8ed9b54f00909aa3216895d301     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:05:20 up 11:49,  1 user,  load average: 0.21, 0.23, 0.19

--- projects/cpu_diff/vsrc/SimTop.v
+++ projects/cpu_diff/vsrc/SimTop.v
@@ -621,7 +621,7 @@ DifftestArchEvent DifftestArchEvent (
     .coreid(0),		// cpu id，单核时固定为0
     .intrNO(inter),		// 中断号，非0时产生中断。产生中断的时钟周期中，DifftestInstrCommit提交的valid需为0
     .cause(0),			// 异常号，ecall时不需要考虑
-    .exceptionPC(EX_pc),	// 产生异常时的PC
+    .exceptionPC(0),	// 产生异常时的PC
     .exceptionInst(EX_instr)	// 产生异常时的指令
 );
 
