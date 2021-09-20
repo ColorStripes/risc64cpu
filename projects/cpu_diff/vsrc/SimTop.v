a95b88b7cdc8373d00b913f559f51a6571bac456
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-20 19:26:59 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 6d388c8ed9b54f00909aa3216895d301     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  19:26:59 up 11:11,  1 user,  load average: 0.47, 0.22, 0.17

--- projects/cpu_diff/vsrc/SimTop.v
+++ projects/cpu_diff/vsrc/SimTop.v
@@ -604,7 +604,7 @@ always @(negedge clock) begin
     cmt_inst <= wb_instr;//
     cmt_valid <= inst_valid;
 		regs_diff <= regs;
-    inter <= time_inter;
+    inter <= (MEM_except_type == 64'h1);
 
     trap <= (wb_instr[6:0] == 7'h6b);      /////////////////////duo  xie  le   wb_instr
     trap_code <= regs[10][7:0];
