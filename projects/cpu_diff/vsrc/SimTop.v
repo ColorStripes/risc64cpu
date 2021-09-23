82dda961f6017e04818b5656b34f4ed9ee61750b
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-23 23:28:17 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: a77f6b732024441ab132111d560d9389     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  23:28:17 up  1:32,  1 user,  load average: 0.36, 0.24, 0.29

--- projects/cpu_diff/vsrc/SimTop.v
+++ projects/cpu_diff/vsrc/SimTop.v
@@ -595,7 +595,7 @@ reg [63 : 0] MEM_except_type_f;
 
 wire inst_valid = ((wb_pc != `PC_START) | (wb_instr != 0)) && ~(inter != 32'h0);
 //wire skip = (wb_instr == 32'h7b) | (wb_csr_addr == 12'hb00) | (MEM_except_type == 64'h2) | (wb_instr == 32'h00063783) | (wb_instr == 32'h00f63023);
-wire skip = (wb_instr == 32'h7b) | (wb_csr_addr == 12'hb00) | (MEM_except_type == 64'h2) | (wb_instr == 32'h0007b483) | (wb_instr == 32'h00f73023);
+wire skip = (wb_instr == 32'h7b) | (wb_csr_addr == 12'hb00) | (MEM_except_type == 64'h2)  | (wb_instr == 32'h00f73023);
 wire cause = (MEM_except_type == 64'h4);
 
 always @(negedge clock) begin
