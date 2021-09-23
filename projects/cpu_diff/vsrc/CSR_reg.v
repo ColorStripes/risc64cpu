137cb45686d160cf8ab9218b12fb4feded904660
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-24 00:52:40 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: a77f6b732024441ab132111d560d9389     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:52:40 up  2:56,  1 user,  load average: 0.20, 0.16, 0.12

--- projects/cpu_diff/vsrc/CSR_reg.v
+++ projects/cpu_diff/vsrc/CSR_reg.v
@@ -59,7 +59,7 @@ module CSR_reg (
         else begin
 
             if(mcycle != 64'hffff_ffff_ffff_ffff) begin
-                csr_mcycle <= mcycle +1;
+                csr_mcycle <= mcycle + 1;
             end
             else begin
                 csr_mcycle <= 64'h0;
@@ -179,7 +179,7 @@ module CSR_reg (
     end
 
 
-assign mstatus = ((csr_w_ena == 1'b1) & (csr_w_addr == `mstatus)) ? {csr_mstatus, csr_w_data[62 : 0]} :   
+assign mstatus = ((csr_w_ena == 1'b1) & (csr_w_addr == `mstatus)) ? csr_mstatus[63] :   
        // ((except_type == 64'h1) | (except_type == 64'h2) | (except_type == 64'h3)) ? 
     //{csr_mstatus[63:13], 2'b11, csr_mstatus[10:8], csr_mstatus[3], csr_mstatus[6:4], 1'b0, csr_mstatus[2:0]} : (except_type == 64'h4) ?
     //{csr_mstatus[63:13], 2'b00, csr_mstatus[10:8], 1'b0, csr_mstatus[6:4], csr_mstatus[7], csr_mstatus[2:0]} :
