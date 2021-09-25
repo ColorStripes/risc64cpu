272965158493b722463b3d80d9f2eeabc070ecf6
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-25 23:01:13 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: b0d4ab054e324411b8cd78c9fe307819     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  23:01:13 up  8:35,  1 user,  load average: 0.83, 0.41, 0.22

--- projects/cpu_diff/vsrc/CSR_reg.v
+++ projects/cpu_diff/vsrc/CSR_reg.v
@@ -74,6 +74,8 @@ module CSR_reg (
                     `mstatus:begin
                         csr_mstatus[62 : 0] <= csr_w_data[62 : 0];
                         csr_mstatus[63] <= (csr_w_data[12] & csr_w_data[13]) | (csr_w_data[14] & csr_w_data[15]);
+                        csr_sstatus[63] <= (csr_w_data[12] & csr_w_data[13]) | (csr_w_data[14] & csr_w_data[15]);
+                        csr_sstatus[16 : 13] <= csr_w_data[16 : 13];
                     end
                      `mtvec:begin
                         csr_mtvec <= csr_w_data;
