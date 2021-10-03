c0544c6844efe5902a434ba34125c24ab3223a64
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-03 14:38:33 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 69c13236df9144bd8820f3d1a140c648     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  14:38:33 up  9:31,  1 user,  load average: 0.31, 0.20, 0.18

--- projects/cpu_axi_diff/vsrc/cpu.v
+++ projects/cpu_axi_diff/vsrc/cpu.v
@@ -155,7 +155,7 @@ DifftestInstrCommit DifftestInstrCommit(
   .clock              (clock),
   .coreid             (0),
   .index              (0),
-  .valid              (0),
+  .valid              (cmt_valid),
   .pc                 (cmt_pc),
   .instr              (cmt_inst),
   .skip               (0),
