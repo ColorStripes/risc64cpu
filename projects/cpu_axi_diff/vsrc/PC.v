91f20aa82f1cc7f9eedeaac6082253fbfe40224d
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-04 16:43:32 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 531fe67f6d61450993d32ef5174816f7     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  16:43:32 up  1:32,  1 user,  load average: 0.31, 0.31, 0.31

---
+++ projects/cpu_axi_diff/vsrc/PC.v
@@ -0,0 +1,49 @@
+//2021.8.3
+//xu xin
+
+
+`include "defines.v"
+
+module PC(
+  input wire clk,
+  input wire rst,
+  input wire [`PC_BUS] pc_i,
+  input wire pc_con,
+  input wire [`PC_BUS] new_pc,
+  input wire flush,	
+  input wire if_ready,                //AXI
+
+  output reg I_M_e,
+  output reg [`PC_BUS] pc
+                           
+);
+wire handshake_done = I_M_e & if_ready;
+
+always@( posedge clk )
+begin
+  if( rst == 1'b1 )
+  begin
+    I_M_e <= 1'b0;
+  end
+  else
+  begin
+    I_M_e <= 1'b1;
+  end
+end
+
+always@( posedge clk ) begin
+  if( I_M_e == 1'b0 ) begin
+    pc <= `PC_START ;
+  end
+  else if( handshake_done ) begin    
+
+    if(flush == 1'b1) begin
+      pc <= new_pc;
+    end
+    else if (pc_con == 1'b0) begin
+      pc <= pc_i;
+    end
+
+  end
+end
+endmodule
