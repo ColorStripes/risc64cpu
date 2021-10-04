d66827cde03719aadf99dc3272d188f79173c210
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-04 17:49:26 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 531fe67f6d61450993d32ef5174816f7     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  17:49:26 up  2:38,  1 user,  load average: 0.29, 0.19, 0.18

--- projects/cpu_axi_diff/vsrc/SimTop.v
+++ projects/cpu_axi_diff/vsrc/SimTop.v
@@ -139,7 +139,7 @@ module SimTop(
         .reset                          (reset),
 
         .rw_valid_i                     (AXI_vaild),
-        .rw_ready_o                     (if_ready),
+        .rw_ready_o                     (AXI_ready),
         .rw_req_i                       (AXI_req),
         .data_read_o                    (AXI_r_data),
         .data_write_i                   (AXI_w_data),
@@ -250,7 +250,7 @@ arbitrate arbitrate (
     .clk(clock),
     .rst(reset),
 
-    .if_ready(),
+    .if_ready(if_ready),
     .if_data_read(if_data_read),
 
     .if_valid(if_valid),
@@ -275,7 +275,7 @@ arbitrate arbitrate (
     .AXI_size(AXI_size),
     .AXI_id(AXI_id),
 
-    .AXI_ready(if_ready),
+    .AXI_ready(AXI_ready),
     .AXI_out_id(AXI_out_id),
     .AXI_r_data(AXI_r_data)
     
