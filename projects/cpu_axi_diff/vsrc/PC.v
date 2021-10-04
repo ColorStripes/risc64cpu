1c61ff523ea524a5ff235242b0f87987db019d80
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-04 16:18:42 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 531fe67f6d61450993d32ef5174816f7     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  16:18:42 up  1:07,  1 user,  load average: 0.55, 0.34, 0.24

--- projects/cpu_axi_diff/vsrc/PC.v
+++ projects/cpu_axi_diff/vsrc/PC.v
@@ -17,25 +17,28 @@ module PC(
   output reg [`PC_BUS] pc
                            
 );
+
+
+
 wire handshake_done = I_M_e & if_ready;
 reg [63:0] addr;
+reg fetched;
 
-
+// fetch an instruction
 always @( posedge clk ) begin
   if (rst) begin
+    //pc <= `PC_START;
     pc <= `PC_START;
-    //if_addr <= `PC_START;
-    //fetched <= 0;
+    fetched <= 0;
   end
   else if ( handshake_done ) begin
+    //pc <= pc;
     pc <= pc + 4;
-    //fetched <= 1;
+    fetched <= 1;
     //inst <= if_data_read[31:0];
   end
   else begin
-    //fetched <= 0;
+    fetched <= 0;
   end
 end
-
-assign I_M_e = 1'b1;
 endmodule
