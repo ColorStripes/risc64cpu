c5d1104adfa8ba9ab2565cc222c8001e3c9acd48
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-04 18:39:30 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 531fe67f6d61450993d32ef5174816f7     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:39:30 up  3:28,  1 user,  load average: 0.47, 0.27, 0.15

--- projects/cpu_axi_diff/vsrc/arbitrate.v
+++ projects/cpu_axi_diff/vsrc/arbitrate.v
@@ -74,11 +74,11 @@ always @(*) begin
     if_data_read = `ZERO_WORD;
     
     
-        if(AXI_out_id == 1) begin
+        if(AXI_out_id == 64'b1) begin
             mem_data = AXI_r_data;
             mem_ready = AXI_ready;
         end
-        else if(AXI_out_id == 3) begin
+        else if(AXI_out_id == 64'b11) begin
             if_data_read = AXI_r_data;
             if_ready = AXI_ready;
         end
