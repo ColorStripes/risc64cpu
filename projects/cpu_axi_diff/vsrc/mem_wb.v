4445b9f34d40e9bcf6783683100ee1983ce5a9ce
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-07 14:06:45 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 531fe67f6d61450993d32ef5174816f7     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  14:06:45 up 1 day,  3:47,  1 user,  load average: 0.69, 0.57, 0.41

--- projects/cpu_axi_diff/vsrc/mem_wb.v
+++ projects/cpu_axi_diff/vsrc/mem_wb.v
@@ -56,8 +56,8 @@ module mem_wb (
                 wb_w_data <= `ZERO_WORD;
                 wb_w_ena <= 1'b0;
                 wb_w_addr <= `ZERO_REG_ADDR;
-                wb_pc <= `PC_START;                             //for difftest
-                wb_instr <= `ZERO_INST;
+                //wb_pc <= `PC_START;                             //for difftest
+                //wb_instr <= `ZERO_INST;
                 wb_csr_addr <= 12'h000;
                 wb_w_csr_data <= `ZERO_WORD;
                 wb_csr_ena <= 1'b0;
