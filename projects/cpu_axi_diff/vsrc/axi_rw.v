1fdd9fad9f909216b5439e76ed4b5308d291a019
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-13 02:03:24 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 809ea6b0d4cf4865bfcaeec7493bf400     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:03:24 up 20:37,  1 user,  load average: 0.19, 0.18, 0.23

--- projects/cpu_axi_diff/vsrc/axi_rw.v
+++ projects/cpu_axi_diff/vsrc/axi_rw.v
@@ -356,7 +356,7 @@ reg axi_b_valid_i_nxt;
     assign axi_ar_qos_o     = 4'h0;
 
     // Read data channel signals
-    assign axi_r_ready_o    = r_state_read;
+    assign axi_r_ready_o    = r_state_read & ~w_valid;
 
     wire [AXI_DATA_WIDTH-1:0] axi_r_data_l  = (axi_r_data_i & mask_l) >> aligned_offset_l;
     wire [AXI_DATA_WIDTH-1:0] axi_r_data_h  = (axi_r_data_i & mask_h) << aligned_offset_h;
