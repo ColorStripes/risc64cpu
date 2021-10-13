c9887ee8cebf63f8b3aa0c713cecbb5a27576f3d
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-13 12:10:37 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 809ea6b0d4cf4865bfcaeec7493bf400     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  12:10:37 up 1 day, 48 min,  1 user,  load average: 0.34, 0.31, 0.41

--- projects/cpu_axi_diff/vsrc/SimTop.v
+++ projects/cpu_axi_diff/vsrc/SimTop.v
@@ -145,7 +145,7 @@ module SimTop(
     assign `AXI_TOP_INTERFACE(ar_bits_id)           = ar_id;
     assign `AXI_TOP_INTERFACE(ar_bits_user)         = ar_user;
     assign `AXI_TOP_INTERFACE(ar_bits_len)          = ar_len;
-    assign `AXI_TOP_INTERFACE(ar_bits_size)         = ar_size;
+    assign `AXI_TOP_INTERFACE(ar_bits_size)         = 011;
     assign `AXI_TOP_INTERFACE(ar_bits_burst)        = ar_burst;
     assign `AXI_TOP_INTERFACE(ar_bits_lock)         = ar_lock;
     assign `AXI_TOP_INTERFACE(ar_bits_cache)        = ar_cache;
