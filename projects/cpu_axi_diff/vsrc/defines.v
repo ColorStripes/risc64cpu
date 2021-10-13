423de9a19dce62e9495ebc85c794bb218577252a
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-13 17:31:34 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 809ea6b0d4cf4865bfcaeec7493bf400     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  17:31:34 up 1 day,  4:49,  1 user,  load average: 0.42, 0.31, 0.30

--- projects/cpu_axi_diff/vsrc/defines.v
+++ projects/cpu_axi_diff/vsrc/defines.v
@@ -12,8 +12,8 @@
 `define ZERO_ENA   1'b0
 `define ZERO_REG_ADDR   5'b00000
 //forecase
-`define FORECASE 64
-`define FORECASE_LOG 6
+`define FORECASE 4
+`define FORECASE_LOG 2
 `define PC 64
 `define PC_LOG 6
 
