3c8c2f6b00a1a1addea81cbd8391cede86273f28
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-13 21:33:08 +0800

    徐鑫 20210457    Static hostname: xu-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: a9c799bf13a64ff78a56aa593b74bf8d            Boot ID: 809ea6b0d4cf4865bfcaeec7493bf400     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:33:08 up 1 day,  8:51,  1 user,  load average: 0.27, 0.30, 0.35

--- projects/cpu_axi_diff/vsrc/ysyx_210457_forecase.v
+++ projects/cpu_axi_diff/vsrc/ysyx_210457_forecase.v
@@ -21,8 +21,8 @@ module ysyx_210457_forecase (
 );
     integer i;
     reg [1 : 0] fore;
-    reg [`PC_BUS] fore_branch[`FORECASE-1 : 0];
-    reg [`PC_BUS] pc_now[8-1 : 0];
+    reg [`PC_BUS] fore_branch[`FORECASE : 0];
+    reg [`PC_BUS] pc_now[`PC-1 : 0];
 
 
 
