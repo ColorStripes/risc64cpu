502f10990d1ca1bf5f61eb2eb3bc27b1316ef66e
tracer-oscpu2021 <tracer@oscpu.org>
2022-07-26 22:37:31 +0800

    徐鑫 20210457    Static hostname: xu          Icon name: computer-vm            Chassis: vm         Machine ID: 53b0cb0147864150a89fc0a9acf38567            Boot ID: 1d73d7a48f2043c2a13a609345c055d3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  22:37:31 up 11:41,  1 user,  load average: 0.43, 0.36, 0.23

--- projects/cpu_axi_diff2/vsrc/SimTop.v
+++ projects/cpu_axi_diff2/vsrc/SimTop.v
@@ -1,5 +1,6 @@
-
+//2022.7.26 xuxin
 `include "defines.v"
+
 `define AXI_TOP_INTERFACE(name) io_memAXI_0_``name
 
 module SimTop(
@@ -64,9 +65,6 @@ module SimTop(
     input  [`AXI_USER_WIDTH-1:0]        `AXI_TOP_INTERFACE(r_bits_user)
 );
 
-initial begin
-        $monitor("率:%d,%d\n", aw_addr,ar_valid);
-end
 
     wire aw_ready;
     wire aw_valid;
@@ -119,12 +117,12 @@ end
 
     assign aw_ready                                 = `AXI_TOP_INTERFACE(aw_ready);
     assign `AXI_TOP_INTERFACE(aw_valid)             = aw_valid;
-    assign `AXI_TOP_INTERFACE(aw_bits_addr)         = {32'h0,aw_addr};
+    assign `AXI_TOP_INTERFACE(aw_bits_addr)         = aw_addr;
     assign `AXI_TOP_INTERFACE(aw_bits_prot)         = aw_prot;
     assign `AXI_TOP_INTERFACE(aw_bits_id)           = aw_id;
     assign `AXI_TOP_INTERFACE(aw_bits_user)         = aw_user;
     assign `AXI_TOP_INTERFACE(aw_bits_len)          = aw_len;
-    assign `AXI_TOP_INTERFACE(aw_bits_size)         = 2'b11;
+    assign `AXI_TOP_INTERFACE(aw_bits_size)         = aw_size;
     assign `AXI_TOP_INTERFACE(aw_bits_burst)        = aw_burst;
     assign `AXI_TOP_INTERFACE(aw_bits_lock)         = aw_lock;
     assign `AXI_TOP_INTERFACE(aw_bits_cache)        = aw_cache;
@@ -143,13 +141,13 @@ end
     assign b_user                                   = `AXI_TOP_INTERFACE(b_bits_user);
 
     assign ar_ready                                 = `AXI_TOP_INTERFACE(ar_ready);
-    assign `AXI_TOP_INTERFACE(ar_valid)             = ar_valid;
-    assign `AXI_TOP_INTERFACE(ar_bits_addr)         = {32'h0,ar_addr};
+    assign `AXI_TOP_INTERFACE(ar_valid)             = ar_valid;                   ////////
+    assign `AXI_TOP_INTERFACE(ar_bits_addr)         = ar_addr;
     assign `AXI_TOP_INTERFACE(ar_bits_prot)         = ar_prot;
     assign `AXI_TOP_INTERFACE(ar_bits_id)           = ar_id;
     assign `AXI_TOP_INTERFACE(ar_bits_user)         = ar_user;
     assign `AXI_TOP_INTERFACE(ar_bits_len)          = ar_len;
-    assign `AXI_TOP_INTERFACE(ar_bits_size)         = 2'b11;
+    assign `AXI_TOP_INTERFACE(ar_bits_size)         = ar_size;
     assign `AXI_TOP_INTERFACE(ar_bits_burst)        = ar_burst;
     assign `AXI_TOP_INTERFACE(ar_bits_lock)         = ar_lock;
     assign `AXI_TOP_INTERFACE(ar_bits_cache)        = ar_cache;
@@ -162,6 +160,79 @@ end
     assign r_last                                   = `AXI_TOP_INTERFACE(r_bits_last);
     assign r_id                                     = `AXI_TOP_INTERFACE(r_bits_id);
     assign r_user                                   = `AXI_TOP_INTERFACE(r_bits_user);
+
+
+initial begin
+        $monitor("率:%d,%d\n", aw_addr, ar_valid);
+end
+
+
+
+// rvcpu rvcpu(
+// .clock(clock),
+// .reset(reset),
+// .io_interrupt(),
+// .io_master_awready(aw_ready),
+// .io_master_awvalid(aw_valid),
+// .io_master_awaddr(aw_addr),
+// .io_master_awid(aw_id),
+// .io_master_awlen(aw_len),
+// .io_master_awsize(aw_size),
+// .io_master_awburst(aw_burst),
+// .io_master_wready(w_ready),
+// .io_master_wvalid(w_valid),
+// .io_master_wdata(w_data),
+// .io_master_wstrb(w_strb),
+// .io_master_wlast(w_last),
+// .io_master_bready(b_ready),
+// .io_master_bvalid(b_valid),
+// .io_master_bresp(b_resp),
+// .io_master_bid(b_id),
+// .io_master_arready(ar_ready),
+// .io_master_arvalid(ar_valid),
+// .io_master_araddr(ar_addr),
+// .io_master_arid(ar_id),
+// .io_master_arlen(ar_len),
+// .io_master_arsize(ar_size),
+// .io_master_arburst(ar_burst),
+// .io_master_rready(r_ready),
+// .io_master_rvalid(r_valid),
+// .io_master_rresp(r_resp),
+// .io_master_rdata(r_data),
+// .io_master_rlast(r_last),
+// .io_master_rid(r_id),
+// .io_slave_awready(),
+// .io_slave_awvalid(),
+// .io_slave_awaddr(),
+// .io_slave_awid(),
+// .io_slave_awlen(),
+// .io_slave_awsize(),
+// .io_slave_awburst(),
+// .io_slave_wready(),
+// .io_slave_wvalid(),
+// .io_slave_wdata(),
+// .io_slave_wstrb(),
+// .io_slave_wlast(),
+// .io_slave_bready(),
+// .io_slave_bvalid(),
+// .io_slave_bresp(),
+// .io_slave_bid(),
+// .io_slave_arready(),
+// .io_slave_arvalid(),
+// .io_slave_araddr(),
+// .io_slave_arid(),
+// .io_slave_arlen(),
+// .io_slave_arsize(),
+// .io_slave_arburst(),
+// .io_slave_rready(),
+// .io_slave_rvalid(),
+// .io_slave_rresp(),
+// .io_slave_rdata(),
+// .io_slave_rlast(),
+// .io_slave_rid()
+
+// );
+
 
 
 
