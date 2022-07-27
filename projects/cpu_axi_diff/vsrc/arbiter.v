e8660319697ee1e6682d83b84fc4fc1bddde746e
tracer-oscpu2021 <tracer@oscpu.org>
2022-07-27 14:22:05 +0800

    徐鑫 20210457    Static hostname: xu          Icon name: computer-vm            Chassis: vm         Machine ID: 53b0cb0147864150a89fc0a9acf38567            Boot ID: 1d73d7a48f2043c2a13a609345c055d3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  14:22:05 up 17:07,  1 user,  load average: 0.57, 0.27, 0.11

--- projects/cpu_axi_diff/vsrc/arbiter.v
+++ projects/cpu_axi_diff/vsrc/arbiter.v
@@ -52,19 +52,22 @@ module arbiter (
 wire axi_fetch_ready = (AXI_ret_id == 1) ? AXI_ready : 1'b0;
 wire pc_to_axi_valid = pc_valid & !arbiter_if_valid;
 assign arbiter_pc_ready = arbiter_if_valid & if_ready;
-always @(posedge clock) begin
-    if(reset) begin
-        arbiter_if_valid <= 0;
-    end
-    else begin
-        if(axi_fetch_ready) begin           //
-            arbiter_if_valid <= 1;
-        end
-        if(arbiter_pc_ready) begin
-            arbiter_if_valid <= 0;
-        end
-    end
-end
+// always @(posedge clock) begin
+//     if(reset) begin
+//         arbiter_if_valid <= 0;
+//     end
+//     else begin
+//         if(axi_fetch_ready) begin           //
+//             arbiter_if_valid <= 1;
+//         end
+//         if(arbiter_pc_ready) begin
+//             arbiter_if_valid <= 0;
+//         end
+//     end
+// end
+assign arbiter_if_valid = axi_fetch_ready;
+
+
 
 
 wire axi_mem_ready = (AXI_ret_id == 11) ? AXI_ready : 1'b0;
@@ -84,21 +87,23 @@ always @(posedge clock) begin
     end
 end
 
-//READ
-always @(posedge clock) begin
-    if(reset) begin
-        if_data <= 0;
-        mem_data <= 0;
-    end
-    else begin
-        if(axi_mem_ready) begin
-            mem_data <= AXI_r_data;
-        end
-        if(axi_fetch_ready) begin
-            if_data <= AXI_r_data[31 : 0];
-        end
-    end
-end
+// //READ
+// always @(posedge clock) begin
+//     if(reset) begin
+//         if_data <= 0;
+//         mem_data <= 0;
+//     end
+//     else begin
+//         if(axi_mem_ready) begin
+//             mem_data <= AXI_r_data;
+//         end
+//         if(axi_fetch_ready) begin
+//             if_data <= AXI_r_data[31 : 0];
+//         end
+//     end
+// end
+assign if_data = AXI_r_data[31 : 0];
+assign mem_data = AXI_r_data;
 
 
 assign AXI_size = ex_to_axi_valid ? mem_size : if_size;
@@ -113,112 +118,6 @@ assign AXI_w_data = mem_stor_data;
 
 
 
-// reg flush_reg;
-// always @(posedge clock) begin
-//     if(reset == 1'b1) begin
-//         flush_reg <= 1'b0;
-//     end
-//     else begin
-//         if(flush) begin
-//             flush_reg <= 1'b1;
-//         end
-//         if(~AXI_stall) begin
-//             flush_reg <= 1'b0;
-//         end
-//     end
-// end
 
-
-// always @(*) begin
-//     if(reset == 1'b1) begin
-//         AXI_addr = ZERO_ADDR;
-//         AXI_w_data = ZERO_WORD;
-//         AXI_vaild = 1'b0;
-//         AXI_req = 1'b0;
-//         AXI_size = 2'b0;
-//         AXI_id = 4'b0000;
-//     end
-//     else begin
-//         AXI_addr = ZERO_ADDR;
-//         AXI_w_data = ZERO_WORD;
-//         AXI_vaild = 1'b0;
-//         AXI_req = 1'b0;
-//         AXI_size = 2'b0;
-//         AXI_id = 4'b0000;
-//         if(mem_valid) begin
-//             AXI_addr = mem_addr;
-//             AXI_w_data = mem_stor_data;
-//             AXI_vaild = mem_valid;
-//             AXI_req = mem_req;
-//             AXI_size = mem_sel;
-//             AXI_id = 4'b0001;
-//         end
-//         else if(if_valid) begin
-//             AXI_addr = if_addr;
-//             AXI_w_data = ZERO_WORD;
-//             AXI_vaild = if_valid;
-//             AXI_req = if_req;
-//             AXI_size = if_size;
-//             AXI_id = 4'b0011;
-//         end
-//     end
-// end
-
-
-
-
-// always @(*) begin
-//     if(reset == 1'b1) begin
-//         mem_data = ZERO_WORD;
-//         if_data_read = ZERO_INST;
-//     end
-//     else begin
-
-//             if(AXI_out_id == 4'b1) begin
-//                 mem_data = AXI_r_data;
-//                 if_data_read = ZERO_INST;
-//             end
-//             else if(AXI_out_id == 4'b11) begin
-//                 if_data_read = AXI_r_data[31 : 0];
-//                 mem_data = ZERO_WORD;
-//             end
-//             else begin
-//                 mem_data = ZERO_WORD;
-//                 if_data_read = ZERO_INST;
-//             end
-//     end
-// end
-
-// always @(*) begin
-//     if(reset == 1'b1) begin
-//         stall = 6'b000000;
-//     end
-//     else begin
-//         stall = 6'b000000;
-//         if(mem_valid & if_valid & ~flush_reg) begin
-//             if(mem_req) begin
-//                 stall = {3'b111, AXI_stall, AXI_stall, 1'b0};
-//             end
-//             else begin
-//                 stall = {5'b11111, 1'b0};
-//                 if(AXI_out_id == 4'b1) begin
-//                     stall = {3'b111, AXI_stall, AXI_stall, 1'b0};
-//                 end
-//             end
-//         end
-//         else if(mem_valid & ~if_valid & ~flush_reg) begin
-//             stall = {{5{AXI_stall}}, 1'b0};
-//         end
-//         else if(if_valid & ~flush_reg) begin
-//             stall = {AXI_stall, AXI_stall, AXI_stall, 3'b0};
-//         end
-//         if(flush) begin
-//             stall = 6'b000000;
-//         end
-//         else if(flush_reg) begin
-//             stall = 6'b111110;
-//         end
-//     end
-// end
 
 endmodule
