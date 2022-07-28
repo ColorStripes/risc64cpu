30ab0e200c3c9bc5c35241ea2069c10b89653bbb
tracer-oscpu2021 <tracer@oscpu.org>
2022-07-28 21:13:31 +0800

    徐鑫 20210457    Static hostname: xu          Icon name: computer-vm            Chassis: vm         Machine ID: 53b0cb0147864150a89fc0a9acf38567            Boot ID: 1d73d7a48f2043c2a13a609345c055d3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  21:13:31 up 1 day, 14:21,  1 user,  load average: 0.39, 0.28, 0.40

--- projects/cpu_axi_diff/vsrc/cputop.v
+++ projects/cpu_axi_diff/vsrc/cputop.v
@@ -520,7 +520,7 @@ wire inst_valid = ((WB_pc != `ysyx_22040931_ZERO_PC) | (WB_instr != 0));// && (i
 ////wire cause = (MEM_except_type == 64'h4);
 wire skip = (WB_instr == 32'h7b);
      initial begin
-      if(inter == 32'h7b) begin
+      if(WB_instr == 32'h7b) begin
          $display("%s\n",regs[10]);
       end
      end
