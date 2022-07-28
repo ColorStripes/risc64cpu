dae36dba2801cfe817d04fad6fb83ec784e6aec9
tracer-oscpu2021 <tracer@oscpu.org>
2022-07-28 11:54:45 +0800

    徐鑫 20210457    Static hostname: xu          Icon name: computer-vm            Chassis: vm         Machine ID: 53b0cb0147864150a89fc0a9acf38567            Boot ID: 1d73d7a48f2043c2a13a609345c055d3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  11:54:45 up 1 day,  5:03,  1 user,  load average: 0.01, 0.05, 0.05

--- projects/cpu_axi_diff/vsrc/if_id.v
+++ projects/cpu_axi_diff/vsrc/if_id.v
@@ -37,7 +37,7 @@ assign if_valid = if_now_valid;
         if(reset == 1'b1) begin
             if_now_valid <= 0;
         end
-        else if(if_ready | stall) begin
+        else if(if_ready) begin
             if_now_valid <= pc_valid;
         end
     end
