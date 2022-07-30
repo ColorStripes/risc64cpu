9fa66bff849d1a31a7d1e24328155f1a359a8b9b
tracer-oscpu2021 <tracer@oscpu.org>
2022-07-30 20:12:14 +0800

    徐鑫 20210457    Static hostname: xu          Icon name: computer-vm            Chassis: vm         Machine ID: 53b0cb0147864150a89fc0a9acf38567            Boot ID: 1d73d7a48f2043c2a13a609345c055d3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  20:12:14 up 3 days,  3:29,  1 user,  load average: 0.17, 0.07, 0.04

--- projects/cpu_axi_diff/vsrc/icache.v
+++ projects/cpu_axi_diff/vsrc/icache.v
@@ -61,6 +61,7 @@ module icache #(
         end
     end
 
+    wire [21 : 0] test = TAG_RAM_WAY0[0];
     //hit
     wire way0_hit = ((TAG_RAM_WAY0[index] == tag) & TAG_RAM_WAY0[index][I_TAG]); 
     wire way1_hit = ((TAG_RAM_WAY1[index] == tag) & TAG_RAM_WAY1[index][I_TAG]);
