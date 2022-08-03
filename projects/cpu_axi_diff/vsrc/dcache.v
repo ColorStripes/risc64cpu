f3402d10e063a8ffb06045a920edab731df04d98
tracer-oscpu2021 <tracer@oscpu.org>
2022-08-04 00:08:59 +0800

    徐鑫 20210457    Static hostname: xu          Icon name: computer-vm            Chassis: vm         Machine ID: 53b0cb0147864150a89fc0a9acf38567            Boot ID: 261b2d9cdfc0451b813f02386d314857     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  00:08:59 up 18:39,  1 user,  load average: 0.24, 0.28, 0.21

--- projects/cpu_axi_diff/vsrc/dcache.v
+++ projects/cpu_axi_diff/vsrc/dcache.v
@@ -80,7 +80,7 @@ module dcache #(
     wire way3_hit = (TAG_RAM_WAY3[index][D_TAG-1 : 0] == tag);
     assign hit = way0_hit | way1_hit | way2_hit | way3_hit;
     //dcache valid
-    assign dcache_mem_valid = write_read ? hit | (arbiter_to_icache_valid & ready) : hit & hit_reg & !next_notvalid;
+    assign dcache_mem_valid = write_read ? hit | (arbiter_to_icache_valid & ready) : hit & hit_reg & next_notvalid;
     assign dcache_ex_ready = dcache_mem_valid & mem_ready;
     reg hit_reg;
     always @(posedge clock) begin
@@ -94,6 +94,7 @@ module dcache #(
 
     reg [D_INDEX-1 : 0] old_index;
     reg old_write_read;
+    reg old_way0_hit, old_way1_hit, old_way2_hit, old_way3_hit;
     always @(posedge clock) begin
         if(reset) begin
             old_index <= 0;
@@ -102,11 +103,21 @@ module dcache #(
         else begin 
             old_index <= index;
             old_write_read <= write_read;
+            old_way0_hit <= way0_hit;
+            old_way1_hit <= way1_hit;
+            old_way2_hit <= way2_hit;
+            old_way3_hit <= way3_hit;
         end
     end
 
-    wire next_notvalid = (old_index != index) | (old_write_read != write_read);
+    //wire next_notvalid = (old_index != index) | (old_write_read != write_read);
                       //read-read              //same index but write-read
+    wire next_notvalid = (old_index == index) & 
+                         (old_write_read == write_read) &
+                         (old_way0_hit == way0_hit) & 
+                         (old_way1_hit == way1_hit) & 
+                         (old_way2_hit == way2_hit) & 
+                         (old_way3_hit == way3_hit);
 
     //read
     wire [DATA_RAM_WIDTH-1 : 0] cache_data = way0_hit ? DATA_WAY0 : 
